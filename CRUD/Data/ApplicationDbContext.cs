d4fe799418ef7db566f488ac42b3c25f84952209
ShroukOuda <s36568961@gmail.com>
2025-06-17 22:58:38 +0300

    Add DbContext

---
+++ CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -1,4 +1,7 @@
 
+using CRUD.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace CRUD
 {
     public class Program
@@ -8,7 +11,9 @@ namespace CRUD
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
